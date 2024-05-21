5927e106bea9b55384b99d81513e3c3202f49569
Abayl-97 <matiasmedina2102@gmail.com>
2024-05-21 12:35:52 -0300

    Agrege metodos a rubro

--- src/Dominio/Rubro.java
+++ src/Dominio/Rubro.java
@@ -1,5 +1,24 @@
 package Dominio;
+import java.util.*;
 
 public class Rubro {
+     private String nombre;
+    private String descripcion;
     
+    public void setNombre(String unNombre){
+        nombre = unNombre;
+        
+    }
+    
+    public void setDescripcion(String unaDescripcion){
+        descripcion = unaDescripcion;
+    }
+    
+    
+            
+    public Rubros(String unNombre,String unaDescripcion ){
+        this.setNombre(unNombre);
+        this.setDescripcion(unaDescripcion);
+    }
+   
 }
