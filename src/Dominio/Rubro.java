eb37b0b151f8afb68eaaa61e4918c74b03cee855
Abayl-97 <matiasmedina2102@gmail.com>
2024-05-23 09:53:41 -0300

    Agregados metorods a rubro, contratista y propietario

--- src/Dominio/Rubro.java
+++ src/Dominio/Rubro.java
@@ -1,5 +1,35 @@
 package Dominio;
 
 public class Rubro {
+    private String nombre;
+    private String descripcion;
+    
+    public Rubro(String unNombre, String unaDescripcion) {
+        this.setNombre(unNombre);
+        this.setDescripcion(unaDescripcion);
+    }
+    
+    public void setNombre(String unNombre){
+        nombre = unNombre;
+        
+    }
+    
+    public void setDescripcion(String unaDescripcion) { 
+        descripcion = unaDescripcion;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public String toString(){
+        return nombre + ":" + descripcion;
+    }
+            
+    
     
 }
