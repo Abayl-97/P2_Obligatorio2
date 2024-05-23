5c56c893867d5c24c1fbaf1ebc0292d4b72909a7
Abayl-97 <matiasmedina2102@gmail.com>
2024-05-23 09:54:22 -0300

    Agregados metorods a rubro, contratista y propietario

--- src/Dominio/Capataz.java
+++ src/Dominio/Capataz.java
@@ -9,5 +9,45 @@ package Dominio;
  * @author matia
  */
 public class Capataz {
+    private String nombre;
+    private String cedula;
+    private int direccion;
+    private int año;
+    
+    public void setNombre(String unNombre){
+        nombre = unNombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setCedula(String unaCedula){
+        cedula = unaCedula;
+    }
+    
+    public String getCedula(){
+        return cedula;
+    }
+    
+    public void setDireccion(int unaDireccion){
+        direccion = unaDireccion;
+    }
+    
+    public int getDireccion(){
+        return direccion;
+    }
+    
+    public void setAño(int unAño){
+        año = unAño;
+    }
+    
+    public int getAño(){
+        return año;
+    }
+
+
+    
+    
     
 }
