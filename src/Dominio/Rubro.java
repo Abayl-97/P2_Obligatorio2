9bfcaf7eb1c9163314b0d2900366f536ec36ded9
Abayl-97 <matiasmedina2102@gmail.com>
2024-05-24 09:34:04 -0300

    Agregado los montos para los rubros

--- src/Dominio/Rubro.java
+++ src/Dominio/Rubro.java
@@ -3,10 +3,12 @@ package Dominio;
 public class Rubro {
     private String nombre;
     private String descripcion;
+    private int monto;
     
     public Rubro(String unNombre, String unaDescripcion) {
         this.setNombre(unNombre);
         this.setDescripcion(unaDescripcion);
+        monto = 0;
     }
     
     public void setNombre(String unNombre){
@@ -17,6 +19,9 @@ public class Rubro {
     public void setDescripcion(String unaDescripcion) { 
         descripcion = unaDescripcion;
     }
+    public void setMonto(int unMonto){
+        monto = unMonto;
+    }
     
     public String getNombre(){
         return nombre;
@@ -24,6 +29,10 @@ public class Rubro {
     
     public String getDescripcion(){
         return descripcion;
+    }
+    
+    public int getMonto(){
+        return monto;
     }
     
     public String toString(){
